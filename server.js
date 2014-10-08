8a5aeeca61c3cd84077aa9d87774cd0d486dce05
concreted <arichuang@gmail.com>
2014-10-08 17:57:04 +0000

    Setup websocket server

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({port: 8080});
 
+var handleMessage = function(message) {
+  console.log('received: %s', message);
+  try {
+    var obj = JSON.parse(message);
+    console.log(obj);
+    // this is gross, change it 
+    this.send('uh huh: ' + JSON.stringify(obj));
+  }
+  catch (e) {
+    this.send('not an object, but fine: ' + message);
+  }
+}
+
 wss.on('connection', function(ws) {
-  ws.on('message', function(message) {
-    console.log('received: %s', message);
-  });
-  ws.send('something');
+  ws.on('message', handleMessage.bind(ws));
+  ws.send('connected!');
 });
+// wscat -c ws://cncrtd-u1204-01.cloudapp.net:8080  
 
 var express = require('express');
 var app = express();
@@ -20,3 +32,7 @@ app.get('/', function(req, res) {
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
 });
+
+repl = require('repl');
+r = repl.start('node> ');
+r.context.wss = wss;
